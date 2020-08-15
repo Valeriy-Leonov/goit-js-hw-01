a28dcdb608b0dbb957105e59d08f5e419a29884b
Valeriy-Leonov <valeoprint@ukr.net>
2020-08-15 03:35:57 +0300

    Initial commit

---
+++ ex_1.js
@@ -0,0 +1,12 @@
+let name = String("whatever");
+let price = 1000000;
+
+const firstMessage = `Choised "${name}", price per one unite is ${price} credits`;
+
+console.log(firstMessage.split(" ", 1));
+
+price = 2000000;
+
+const secondMessage = `Choised "${name}", price per one unite is ${price} credits`;
+
+console.log(secondMessage);
